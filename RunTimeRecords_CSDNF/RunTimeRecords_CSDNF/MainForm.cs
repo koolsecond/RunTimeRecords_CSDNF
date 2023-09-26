5e1cdc0ba094f6b50e536c50b1c0f216a2d89ef6
koolsecond <naoki.nishikawa_work@outlook.jp>
2023-09-26 21:59:15 +0900

    プロセス起動時間の取得 #3

--- RunTimeRecords_CSDNF/RunTimeRecords_CSDNF/MainForm.cs
+++ RunTimeRecords_CSDNF/RunTimeRecords_CSDNF/MainForm.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Management; //あらかじめ「System.Management」を参照に追加しておくこと
 using System.Windows.Forms;
-using System.Xml.Linq;
 
 namespace RunTimeRecords_CSDNF
 {
@@ -16,17 +13,16 @@ namespace RunTimeRecords_CSDNF
         public MainForm()
         {
             InitializeComponent();
+            // TODO : ★未実装機能は非表示
+            menuStrip.Visible = false;
+            statusStrip.Visible = false;
+            toolStripStatusLabel1.Text = string.Empty;
+            // プロセスを取得する際に利用するインスタンスを取得しておく。
             // https://learn.microsoft.com/ja-jp/windows/win32/cimwin32prov/win32-process
             _managementClass = new ManagementClass("Win32_Process");
             // データテーブルの初期化
             InitializeProcessDataTable();            
-            // TODO : 未実装機能は非表示
-            menuStrip.Visible = false;
-            statusStrip.Visible = false;
-            toolStripStatusLabel1.Text = string.Empty;
-            // test
-            testLabel.Text = DateTime.Now.ToString();
-            // プロセスリストの初期化
+            // プロセスデータテーブルの初期化
             GetProcessList();
             // プロセスリストの初期化
             SetProcessList();
@@ -75,31 +71,10 @@ namespace RunTimeRecords_CSDNF
                 DataType = typeof(string)
             };
             processDataTable.Columns.Add(column);
-            // UserModeTime 項目の設定
+            // CreationDate 項目の設定
             column = new DataColumn
             {
-                ColumnName = "UserModeTime",
-                DataType = typeof(string)
-            };
-            processDataTable.Columns.Add(column);
-            // KernelModeTime 項目の設定
-            column = new DataColumn
-            {
-                ColumnName = "KernelModeTime",
-                DataType = typeof(string)
-            };
-            processDataTable.Columns.Add(column);
-            // CpuTime 項目の設定
-            column = new DataColumn
-            {
-                ColumnName = "CpuTime",
-                DataType = typeof(string)
-            };
-            processDataTable.Columns.Add(column);
-            // StartTime 項目の設定
-            column = new DataColumn
-            {
-                ColumnName = "StartTime",
+                ColumnName = "CreationDate",
                 DataType = typeof(DateTime)
             };
             processDataTable.Columns.Add(column);
@@ -107,7 +82,7 @@ namespace RunTimeRecords_CSDNF
             column = new DataColumn
             {
                 ColumnName = "RunTime",
-                DataType = typeof(string)
+                DataType = typeof(TimeSpan)
             };
             processDataTable.Columns.Add(column);
         }
@@ -119,20 +94,20 @@ namespace RunTimeRecords_CSDNF
         {
             var nowTime = DateTime.Now;
             // プロセス一覧を取得して設定
-            var list = _managementClass.GetInstances();
-            foreach (var process in list)
+            ManagementObjectCollection list = _managementClass.GetInstances(); // このインスタンスは都度再生成が必要
+            foreach (ManagementBaseObject process in list)
             {
                 try
                 {
-                    var name = process["Name"].ToString();
-                    var id = process["ProcessId"].ToString();
+                    string name = process["Name"].ToString();
+                    string id = process["ProcessId"].ToString();
                     string key = id + name;
                     string path = "";
                     if (process["ExecutablePath"] != null)
                     {
                         path = process["ExecutablePath"].ToString();
                     }
-                    // ★とりま「D:」始まりでないものはスキップ
+                    // TODO : ★とりま「ExecutablePath」が「D:」始まりでないものはスキップ
                     if (!path.StartsWith("D:"))
                     {
                         continue;
@@ -143,58 +118,36 @@ namespace RunTimeRecords_CSDNF
                     {
                         description = process["Description"].ToString();
                     }
-                    // UserModeTime
-                    // ユーザー モードの時間 (100 ナノ秒単位)。
-                    // この情報が使用できない場合は、0 (ゼロ) の値を使用します。
-                    string userModeTime = "";
-                    if (process["UserModeTime"] != null)
-                    {
-                        userModeTime = process["UserModeTime"].ToString();                        
-                    }
-                    // KernelModeTime
-                    // カーネル モードの時間 (100 ナノ秒単位)。
-                    // この情報が使用できない場合は、0 (ゼロ) の値を使用します。
-                    string kernelModeTime = "";
-                    if (process["KernelModeTime"] != null)
-                    {
-                        kernelModeTime = process["KernelModeTime"].ToString();
-                    }
-                    // CPU Time = KernelModeTime + UserModeTime
-                    string cpuTime = "";
-                    if (UInt64.TryParse(userModeTime, out ulong uTime) && UInt64.TryParse(kernelModeTime, out ulong kTime))
-                    {
-                        cpuTime = (uTime + kTime).ToString();
-                    }
-                    // 時刻表示切替
-                    userModeTime = TimeFormatString(StringToTimeSpan(userModeTime));
-                    kernelModeTime = TimeFormatString(StringToTimeSpan(kernelModeTime));
-                    cpuTime = TimeFormatString(StringToTimeSpan(cpuTime));
+                    // CreationDate
+                    string creationDateString = process["CreationDate"].ToString();
+                    int year = int.Parse(creationDateString.Substring(0, 4));
+                    int month = int.Parse(creationDateString.Substring(4, 2));
+                    int day = int.Parse(creationDateString.Substring(6, 2));
+                    int hour = int.Parse(creationDateString.Substring(8, 2));
+                    int minute = int.Parse(creationDateString.Substring(10, 2));
+                    int second = int.Parse(creationDateString.Substring(12, 2));
+                    var creationDate = new DateTime(year, month, day, hour, minute, second);
+                    // RunTime
+                    TimeSpan runTime = nowTime - creationDate;
+
                     // データテーブルへ反映
-                    var newRow = processDataTable.NewRow();
-                    newRow["Key"] = key;
-                    newRow["Name"] = name;
-                    newRow["Id"] = id;
-                    newRow["ExecutablePath"] = path;
-                    newRow["Description"] = description;
-                    newRow["UserModeTime"] = userModeTime;
-                    newRow["kernelModeTime"] = kernelModeTime;
-                    newRow["cpuTime"] = cpuTime;
-                    var existRow = processDataTable.Rows.Find(key);
+                    DataRow existRow = processDataTable.Rows.Find(key);
                     if( existRow != null )
                     {
-                        // 既存データ
-                        existRow["UserModeTime"] = userModeTime;
-                        existRow["kernelModeTime"] = kernelModeTime;
-                        existRow["cpuTime"] = cpuTime;
-                        DateTime startTime = (DateTime)existRow["StartTime"];
-                        TimeSpan runTime = nowTime - startTime;
-                        existRow["RunTime"] = TimeFormatString(runTime);
+                        // 既存データは実行時間のみ更新                        
+                        existRow["RunTime"] = runTime;
                     }
                     else
                     {
                         // 新規データ
-                        newRow["StartTime"] = DateTime.Now;
-                        newRow["RunTime"] = "00:00:00";
+                        DataRow newRow = processDataTable.NewRow();
+                        newRow["Key"] = key;
+                        newRow["Name"] = name;
+                        newRow["Id"] = id;
+                        newRow["ExecutablePath"] = path;
+                        newRow["Description"] = description;
+                        newRow["CreationDate"] = creationDate;
+                        newRow["RunTime"] = runTime;
                         processDataTable.Rows.Add(newRow);
                     }
                 }
@@ -219,32 +172,12 @@ namespace RunTimeRecords_CSDNF
                     (string)row["Id"],
                     (string)row["ExecutablePath"],
                     (string)row["Description"],
-                    ((DateTime)row["StartTime"]).ToString(),
-                    (string)row["RunTime"],
-                    (string)row["UserModeTime"],
-                    (string)row["kernelModeTime"],
-                    (string)row["cpuTime"]
+                    ((DateTime)row["CreationDate"]).ToString(),
+                    TimeFormatString((TimeSpan)row["RunTime"])
                 };
                 processListView.Items.Add(new ListViewItem(item));
             }
         }
-
-        /// <summary>
-        /// TimeSpanとして戻す
-        /// </summary>
-        /// <returns>TimeSpan</returns>
-        private static TimeSpan StringToTimeSpan(string strTime)
-        {
-            if (UInt64.TryParse(strTime, out var time) )
-            {
-                // 1ナノ秒 = 10^-3マイクロ秒 = 10^-6ミリ秒 = 10^-9秒
-                // 100ナノ秒 =  0.1マイクロ秒 * 1000 =  0.1ミリ秒 * 10 = 1ミリ秒
-                var milliSseconds = (int)(time / (Math.Pow(10, 3))); // ミリ秒変換
-                return TimeSpan.FromMilliseconds(milliSseconds);
-            }
-            return new TimeSpan();
-        }
-
 
         private static string TimeFormatString(TimeSpan timeSpan)
         {
@@ -259,7 +192,6 @@ namespace RunTimeRecords_CSDNF
         void OnTimerTick(object sender, EventArgs e)
         {
             GetProcessList(); // 事前に値を取得
-            testLabel.Text = DateTime.Now.ToString();// test
             SetProcessList(); // 取得した値で差し替え
         }
 
